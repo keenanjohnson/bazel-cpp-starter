e66bc79667c16c8ebfa040474105cafa1107eadf
Keenan Johnson <keenan.johnson@gmail.com>
2023-05-31 13:33:36 -0700

    Add test lib

---
+++ lib/thing.cpp
@@ -0,0 +1,9 @@
+#include "thing.h"
+
+
+uint8_t Thing::doStuff()
+{
+	uint8_t test = 1 + 1;
+
+    return test;
+}
---
+++ lib/thing.h
@@ -0,0 +1,14 @@
+#ifndef THING_H
+#define THING_H
+
+
+#include <stdint.h>
+
+class Thing
+{
+    public:
+        uint8_t doStuff();
+};
+
+
+#endif
